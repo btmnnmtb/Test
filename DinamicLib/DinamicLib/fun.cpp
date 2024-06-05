2f0941140d085bf48f0f5cb9db3086f73478c4ff
Batman(Квашнин) <124182220+btmnnmtb@users.noreply.github.com>
2024-06-05 13:17:32 +0300

    Add files via upload

---
+++ DinamicLib/DinamicLib/fun.cpp
@@ -0,0 +1,16 @@
+﻿// dllmain.cpp : Определяет точку входа для приложения DLL.
+#include "pch.h"
+#include <string>
+using namespace std;
+
+
+extern "C" __declspec(dllexport) bool AllCharacters(const char* str, const char* chars) {
+    string s(str);
+    string c(chars);
+    for (char ch : c) {
+        if (s.find(ch) == string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
---
+++ DinamicLib/bibl/pr.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <string>
+
+using namespace std;
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    string iString, sChars;
+    cout << "Введите строку: ";
+    getline(cin, iString);
+    cout << "Введите символы для поиска: ";
+    getline(cin, sChars);
+    HINSTANCE load = LoadLibrary(L"DinamicLib.dll");
+    if (!load) {
+        cout << "Не удалось загрузить библиотеку DinamicLib.dll" << endl;
+        return 1;
+    }
+    typedef bool (*ContainsAllCharactersFunc)(const char*, const char*);
+    ContainsAllCharactersFunc AllCharacters = (ContainsAllCharactersFunc)GetProcAddress(load, "AllCharacters");
+    if (!AllCharacters) {
+        cout << "Не удалось найти функцию AllCharacters" << endl;
+        FreeLibrary(load);
+        return 1;
+    }
+    bool result = AllCharacters(iString.c_str(), sChars.c_str());
+    if (result) {
+        cout << "Да" << endl;
+    }
+    else {
+        cout << "Нет" << endl;
+    }
+    FreeLibrary(load);
+}
